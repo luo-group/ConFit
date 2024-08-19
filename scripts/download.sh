7f5a453e4ff3ffc37a2cd04449cd571973382fd9
Junming Zhao <20307110324@fudan.edu.cn>
2024-08-20 00:34:16 +0800

    fix download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# fix the downloading issues before
 wget --no-check-certificate "https://docs.google.com/uc?export=download&id=1jF2weMxWEi4AolGW1OT73wRsudI3i3zZ" -O "data.zip"
 unzip data.zip
 rm data.zip
